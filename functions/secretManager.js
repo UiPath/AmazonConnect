7c43f9a49c138c9acc609bc43af27fb8ae3c4c37
Paras Gera <paras.gera@uipath.com>
2020-05-27 15:04:26 +0530

    Fixing secretManager

--- functions/secretManager.js
+++ functions/secretManager.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 const client = new AWS.SecretsManager();
 
-export async function getSecret(secretId, callback) {
+async function getSecret(secretId, callback) {
     return new Promise((resolve, reject) => {
         client.getSecretValue({ SecretId: secretId }, function (err, data) {
             if (err) {
@@ -13,7 +13,7 @@ export async function getSecret(secretId, callback) {
     });
 }
 
-export async function updateSecret(secretId, secretValue) {
+async function updateSecret(secretId, secretValue) {
     return new Promise((resolve, reject) => {
         client.updateSecret({ SecretId: secretId, SecretString: secretValue }, function (error, data) {
             if (error) {
@@ -24,3 +24,8 @@ export async function updateSecret(secretId, secretValue) {
         });
     });
 }
+
+module.exports = {
+    getSecret,
+    updateSecret
+};
