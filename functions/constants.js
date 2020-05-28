c64f1a103c98a8be426c5eb2b47c2121a6aff04f
Paras Gera <paras.gera@uipath.com>
2020-05-28 11:33:28 +0530

    Fixing const file

--- functions/constants.js
+++ functions/constants.js
@@ -1,3 +1,9 @@
-export const SOURCE = 'AmazonConnect';
-export const FOLDER_TYPE_CLASSIC = 'classic';
-export const FOLDER_TYPE_MODERN = 'modern';
+const SOURCE = 'AmazonConnect';
+const FOLDER_TYPE_CLASSIC = 'classic';
+const FOLDER_TYPE_MODERN = 'modern';
+
+module.exports = {
+    SOURCE,
+    FOLDER_TYPE_CLASSIC,
+    FOLDER_TYPE_MODERN
+}
